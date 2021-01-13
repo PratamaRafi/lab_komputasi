d6f1df09b296cb1abea345621c56369afecbe720
Pratama Rafi <muh.rafi.pratama@gmail.com>
2021-01-13 11:01:49 +0700

    build OOP in News

--- oprec.php
+++ oprec.php
@@ -1,15 +1,5 @@
 <?php
-$asisten = array( array("No"=>1,"Nama"=>"Anas Fatur Rahman", "NIM"=>"08021181823085 " , "Angkatan"=>"Fisika 2018"),
-               array("No"=>2,"Nama"=>"Ade Rizki Wahyudi", "NIM"=>"08021381924056" , "Angkatan"=>"Fisika 2019"),
-               array("No"=>3  ,"Nama"=>"Tiara Martika Gerhany", "NIM"=>"08021381823074" , "Angkatan"=>"Fisika 2018"),
-               array("No"=>4,"Nama"=>"M. Fahrezy Putra", "NIM"=>"08021281924033" , "Angkatan"=>"Fisika 2019"),
-               array("No"=>5  ,"Nama"=>"Putri Anisah", "NIM"=>"08021181924010", "Angkatan"=>"Fisika 2019"),
-               array("No"=>6,"Nama"=>"M. Ferdinan Yuda", "NIM"=>"08021181924006", "Angkatan"=>"Fisika 2019"),
-               array("No"=>7  ,"Nama"=>"M. Aldi Kurniawan", "NIM"=>"08021281823087", "Angkatan"=>"Fisika 2018"),
-               array("No"=>8,"Nama"=>"Daula Fadlun", "NIM"=>"08021281924032", "Angkatan"=>"Fisika 2019"),
-               array("No"=>9  ,"Nama"=>"Ayu Kirani Azzahra", "NIM"=>"08021381924059	", "Angkatan"=>"Fisika 2019"), 
-             ); 
-$jumlah = 1
+  require_once('dataoprec.php')
 ?>
 
 <!doctype html>
